e6bc07453d1f28a5917a4fe2ef74f968bf0927e3
Abdullah AlZahabi <a.alzahabi@ejad.solutions>
2025-01-05 14:07:42 +0300

    Done Implement Project

---
+++ js/swiper.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper-most-reading", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    autoplay: {
+      delay: 3000,
+    },
+
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+    spaceBetween: 16,
+  });
+});
